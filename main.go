b50a35426cf828b5c8802b262cb7e81b7d5eea84
彭业昌 <refrain@douyu.tv>
2025-12-25 11:00:26 +0800

    Update greeting message in main.go to reflect 'change to gitlab'

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		message := fmt.Sprintf("Argo CD! This is UPDATED VERSION: %s !!! auto change", version)
+		message := fmt.Sprintf("Argo CD! This is UPDATED VERSION: %s !!! change to gitlab", version)
 		fmt.Fprintf(w, message)
 		fmt.Println("Request received:", message)
 	})
